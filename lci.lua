d0c0edd957a5b5bdb2ac94884694057cc0aeaced
Bryan <dogescript@gmail.com>
2016-07-21 21:59:02 -0600

    Bare

---
+++ lci.lua
@@ -0,0 +1,49 @@
+-- DISCLAIMER: This specific implementation was designed to work with
+-- CC Lua, I do not take any responsibility for anything that may happen
+-- in or out of the CC environment. But it shouldn't not work in other envs
+
+local version = 0.01
+
+function resolveFile(file)
+	return ((file:sub(1, 1) == "/" or file:sub(1, 1) == "\\") and file) or fs.combine(workingDir, file)
+end
+
+local requireCache = {}
+
+function require(file)
+    local path = resolveFile(file)
+
+    if requireCache[path] then
+        return requireCache[path]
+    else
+        local env = {
+            shell = shell,
+            require = require,
+            resolveFile = resolveFile
+        }
+
+        setmetatable(env, { __index = _G })
+
+        local chunk, err = loadfile(path, env)
+
+        if chunk == nil then
+            return error("Error loading file " .. path .. ":\n" .. (err or "N/A"), 0)
+        end
+
+        requireCache[path] = chunk()
+        return requireCache[path]
+    end
+end
+
+--Speshul functions that should be native ;p
+
+--Switch implementation taken from http://lua-users.org/wiki/SwitchStatement
+Default, Nil = {}, setmetatable({}, {__call = function () end}) -- for uniqueness
+function switch (i)
+	return setmetatable({ i }, {
+		__call = function (t, cases)
+			local item = #t == 0 and Nil or t[1]
+			return (cases[item] or cases[Default] or Nil)(item)
+		end
+	})
+end
